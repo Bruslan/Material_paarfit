4308233260a1898727684c3486e69f7f091b001e
Bruslan <bruslan@Bruslans-MacBook-Pro.local>
2018-09-26 00:26:54 +0200

    first commit

---
+++ js/newInit.js
@@ -0,0 +1,7 @@
+(function($) {
+  $(function() {
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
+    M.AutoInit();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
